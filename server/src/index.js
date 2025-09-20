c02f0b7f76a67b5fba9de737e9b657e281d0fbec
SUDHANSHU DUBEY <manudubey249@gmail.com>
2025-09-20 08:41:52 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ import { seedDefaultAdmin } from './utils/seedAdmin.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN || 'https://internshala-project-9mc3.vercel.app', credentials: true }));
 app.use(express.json({ limit: '2mb' }));
 app.use(cookieParser());
 
